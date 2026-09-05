cd774bf9bfb5c703febcedd08b0e226509935006
X11un <a9091971@gmail.com>
2026-09-05 03:05:59 +0200

    Update: parse TCP and drop traffic to VIP on port 8080

--- src/lb.bpf.c
+++ src/lb.bpf.c
@@ -2,12 +2,13 @@
 #include <linux/if_ether.h>
 #include <linux/ip.h>
 #include <linux/in.h>
+#include <linux/tcp.h>
 
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
 
 
-// Parser Ethernet & IPV4
+#define LB_VIP 0x0AC80064                       // 10.200.0.100
 
 SEC("xdp")
 int xdp_lb(struct xdp_md *ctx){
@@ -15,6 +16,8 @@ int xdp_lb(struct xdp_md *ctx){
     void *data = (void *)(long)ctx->data;
     void *data_end = (void *)(long)ctx->data_end;
 
+
+// Parser Ethernet    
     struct ethhdr *eth = data;
 
     if ((void*)(eth + 1) > data_end)
@@ -24,17 +27,37 @@ int xdp_lb(struct xdp_md *ctx){
         return XDP_PASS;
 
 
+// IPV4    
     struct iphdr *ip = (void *)(eth + 1);       // Header IPV4 after Ethernet
 
     if ((void *)(ip + 1) > data_end)            // Bounds check if exceed data_end stop
         return XDP_ABORTED;
 
     if (ip->protocol == IPPROTO_ICMP)           // 1 = ICMP
+        return XDP_PASS;
+
+    
+    __u32 ip_header_len = ip->ihl * 4;
+
+    if (ip_header_len < sizeof(*ip))
+        return XDP_ABORTED;
+
+    if ((void *)((char *)ip + ip_header_len) > data_end)
+        return XDP_ABORTED;
+
+    
+// TCP 
+    struct tcphdr *tcp = (void *)((char *)ip + ip_header_len);
+
+    if ((void *)(tcp + 1) > data_end)
+        return XDP_ABORTED;
+
+    if (ip->daddr == bpf_htonl(LB_VIP) && tcp->dest == bpf_htons(8080)){        // VIP 10.200.0.100:8080
         return XDP_DROP;
+    }
 
 
     return XDP_PASS;
 }
-
 
 char LICENSE[] SEC("license") = "GPL";
